ad2cd01d3467c5216c7dae872a9afaa6acd74be2
jeewoou <51470001+jjwoou9@users.noreply.github.com>
2022-08-06 21:44:48 +0900

    revert

--- user-service/src/main/java/com/amp/userservice/jpa/UserEntity.java
+++ user-service/src/main/java/com/amp/userservice/jpa/UserEntity.java
@@ -2,29 +2,28 @@ package com.amp.userservice.jpa;
 
 import javax.persistence.*;
 
-import com.amp.userservice.model.Role;
+import lombok.Data;
 
 import java.io.Serializable;
 
-@Deprecated
+@Data
+@Entity
+@Table(name="users")
 public class UserEntity implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false, length = 50, unique = true)
 	private String email;
-	
+
 	@Column(nullable = false, length = 50)
 	private String name;
-	
+
 	@Column(nullable = false, unique = true)
 	private String userId;
-	
+
 	@Column(nullable = false)
 	private String encryptedPwd;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private Role role;
 }
--- user-service/src/main/java/com/amp/userservice/model/Role.java
+++
@@ -1,5 +0,0 @@
-package com.amp.userservice.model;
-
-public enum Role {
-    ROLE_USER , ROlE_ADMIN, ROLE_MANAGER;
-}
